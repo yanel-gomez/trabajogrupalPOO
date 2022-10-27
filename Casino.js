ae56e60404a9abd9c248592ac648e9e0069f1ea9
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-10-27 08:29:47 -0300

    ejercicio dados

---
+++ Casino.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.Casino = void 0;
+var Casino = /** @class */ (function () {
+    function Casino(nombre) {
+        this.nombre = nombre;
+    }
+    Casino.prototype.seleccionarJuego = function () {
+    };
+    Casino.prototype.setApostar = function () {
+    };
+    Casino.prototype.getResultados = function () {
+    };
+    Casino.prototype.probabilidadDeGanar = function () {
+    };
+    return Casino;
+}());
+exports.Casino = Casino;
--- Dados.ts
+++ Dados.ts
@@ -1,24 +1,73 @@
 import { Casino } from "./Casino";
 
 export class Dados extends Casino{
-    caraDelDado:number;
-    constructor(nombre:string){
-    super(nombre) 
+   private dado1:number;
+   private dado2:number;
+   private apuesta:number;
+
+    constructor(nombre:string,apuesta:number){
+     super(nombre) 
+     this.dado1 = 0;
+     this.dado2 = 0;
+     this.apuesta = apuesta;
     }
 
-tirarDados(){
+    public tirarDados():void{
+      this.dado1 = Math.floor(Math.random() * (6 - 1)) + 1;
+      this.dado2 = Math.floor(Math.random() * (6 - 1)) + 1;
+    }
+    public sumarDados():number{
+     let suma :number = this.dado1 + this.dado2;
+     return suma
+    }
+    seleccionarJuego(){
 
-}
-sumarDados(){
+    }
+    public apostar(plata:number){
+        this.apuesta = plata + this.apuesta
+    }
+    public verResultados():string{
+    let respuesta :string = "No ganaste , intenta de nuevo ";
+        if(this.dado1 === this.dado2)
+             respuesta = "Ganaste: "+ this.apuesta * 2 +" Felicidades ";
+        return respuesta
+    }
+    
+    
+    public intentos(apuesta:number){
+    let intentos: number = 0;
+    let continuar: boolean = true
+    
+     while (intentos < 3 && continuar === true) {
+    if (this.sumarDados() === apuesta) {
+        console.log("Felicitaciones ganaste");
+        continuar = false;
+    } else {
+        console.log("No ganaste nada");
+    }
+    intentos++;
+    }
+    if (continuar === true) {
+    console.log("acabaste los intentos");
+    }
+    }
+    
+   
+    public probabilidadDeGanar(): void {
+    let n : number = 1; //cantidad de veces que tira
+    let probCara : number = 1/36; //probabilidad de que salga una cara del dado
+    let probFinal : number = 1;
 
+    for (let i: number = 0; i <= n; i++) {
+    probFinal = probFinal * probCara;
+    }
+    console.log ("La probabilidad es: ", probFinal );
+    }
 }
-seleccionarJuego(){
 
-}
-apostar(){
+let pepe : Dados = new Dados("Nico",1000);
 
-}
-verResultados(){
-
-}
-}
+pepe.apostar(1000);
+pepe.tirarDados();
+pepe.sumarDados();
+pepe.probabilidadDeGanar();
